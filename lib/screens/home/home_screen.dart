4c8fc8af0523eaf8756fae0fe47c4235d2dd2450
Fabricyo <bass.barreto@gmail.com>
2026-04-10 14:33:07 -0300

    style: datepicker in home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/material.dart';
 import 'package:tarefas/screens/home/top_bar.dart';
 
@@ -9,8 +10,27 @@ class HomeScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         body: Padding(
-          padding: const EdgeInsets.only(top: 20.0, left: 15.0, right: 15.0),
-          child: Column(children: [TopBar()]),
+          padding: EdgeInsets.only(top: 20.0, left: 15.0, right: 15.0),
+          child: Column(
+            children: [
+              TopBar(),
+              Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFFD2E1E9),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: DatePicker(
+                  DateTime.now(),
+                  height: 88,
+                  initialSelectedDate: DateTime.now(),
+                  selectionColor: Theme.of(context).colorScheme.primary,
+                  selectedTextColor: Colors.white,
+                  locale: 'pt_br',
+                  daysCount: 8,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
